581d9a7ebdd947e332b24db0c77f2d3a8ce70153
yoonhoelee <ubrain0624@gmail.com>
2022-05-07 16:04:12 +0900

    initial change
    initial change

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+    let sex = 'sex';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hello React (sex)</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
